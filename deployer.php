633b935e00f3d1005e1477efd9bee2f63aee581f
bmcculley <bmcculley@ejank.com>
2022-07-09 01:31:48 -0400

    Enable reading GitHub payload
    The GitHub payload is sent as an HTTP body and url encoded. 
    It first needs to be parsed and decoded before it can be parsed as JSON.

--- deployer.php
+++ deployer.php
@@ -1,6 +1,7 @@
 <?php
 $content = file_get_contents("php://input");
-$json    = json_decode($content, true);
+parse_str(urldecode($content), $result);
+$json    = json_decode($result["payload"], true);
 $file    = fopen(LOGFILE, "a");
 $time    = time();
 $token   = false;
